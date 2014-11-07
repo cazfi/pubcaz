56b41744a9f93282e395161e87a50b9ce255bd7f
Marko Lindqvist <cazfi74@gmail.com>
2014-11-07 02:28:39 +0200

    cron_run.sh: Add
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

---
+++ cron_run.sh
@@ -0,0 +1,33 @@
+#!/bin/dash
+
+cd $(dirname $0)
+
+if test "x$1" = "x-h" || test "x$1" = "x--help" || test "x$1" = "x" ; then
+  echo "Usage: $0 <server version> [port] [ruleset] [extra config] [scenario]"
+  exit 0
+fi
+
+LANG="en_US.UTF-8"
+export LANG
+FREECIV_LOCAL_ENCODING="UTF-8"
+export FREECIV_LOCAL_ENCODING
+
+if test "x$2" = "x" ; then
+  PORT=5556
+else
+  PORT="$2"
+fi
+
+umask 0002
+
+if test -f servers.stop || test -f portflags/server.$PORT || test -f cron.disabled ; then
+  exit 0
+fi
+
+touch portflags/server.$PORT
+
+if ! ./run_server.sh "$1" "$PORT" on "$3" "$4" "$5" >/dev/null; then
+  echo "Freeciv server run failed (\"$1\" \"$PORT\")" >&2
+fi
+
+rm portflags/server.$PORT
