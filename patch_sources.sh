2192984d78b38e7ca7ad7758891363130cd71abd
Marko Lindqvist <cazfi74@gmail.com>
2022-05-29 22:59:34 +0300

    patch_sources.sh: Filter more files
    Filter windows directory, and .gitignore files
    
    Signed-off-by: Marko Lindqvist <cazfi74@gmail.com>

--- patch_sources.sh
+++ patch_sources.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-export FILTEREDTYPES=".png .svg .ogg .ttf .spec .tilespec .jpg .po .git ChangeLog setup_auth_server.sh"
-export FILTEREDDIRS="client themes doc graphics .git"
+export FILTEREDTYPES=".png .svg .ogg .ttf .spec .tilespec .jpg .po .git ChangeLog setup_auth_server.sh .gitignore"
+export FILTEREDDIRS="client themes doc graphics .git windows"
 
 # $1 - Version
 # $2 - Patch
